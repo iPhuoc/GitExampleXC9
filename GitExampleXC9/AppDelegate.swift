15358ea004b9d1ab681d85a6972b5412ee5db80e
Phuoc Le <phuocle@hotmail.de>
2018-02-22 19:59:22 +0100

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by Khac Phuoc Le on 22.02.18.
+//  Copyright © 2018 Khac Phuoc Le. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
